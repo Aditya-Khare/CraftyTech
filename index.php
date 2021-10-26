8241159703f760ef16c4942c18999113a27ea9a9
Aditya Khare <81507533+Aditya-Khare@users.noreply.github.com>
2021-10-26 11:52:24 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- @author : ADITYA KHARE - 70021118067 -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="favicon.ico">
+        <title>CraftyTech</title>
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="assets/css/main.css" rel="stylesheet">
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="assets/js/modernizr.custom.js"></script>
+        <style>
+            .menu a {
+			color: #fff;
+			text-align: center;
+			/*margin-right: 5px;
+			margin-left: 5px;
+			margin-top: 2px;*/
+			padding: 2px;
+			text-decoration: none;
+			font-size: 10px;
+			height: fit-content;
+		}
+            .menu a.active {
+			background-color: #4158d0;
+			background: -webkit-linear-gradient(-135deg, #c850c0, #4158d0);
+			border: 1px solid black;
+			border-radius: 10px;
+			color: white;
+		}
+        </style>
+    </head>
+
+    <body>
+        <!-- Menu -->
+        <nav class="menu" id="theMenu">
+            <div class="menu-wrap">
+                <h1 class="logo"><a href="index.html#home">CraftyTech</a></h1>
+                <i class="fa fa-arrow-right menu-close"></i>
+                <a class="active" href="index.html">Home</a><br>
+                <!-- <a href="">Projects</a> -->
+                <a href="./ChatApp/login.php">Chat</a>
+                <a href="games.html">Games</a>
+                <a href="./orderonline/orderonline.html">Order Food</a>
+                <a href="./Quotes/quotes.html">Quotes</a>                
+            </div>
+            <!-- Menu button -->
+            <div id="menuToggle"><i class="fa fa-bars"></i></div>
+        </nav>
+<!-- @author : ADITYA KHARE - 70021118067 -->
+        <!-- MAIN IMAGE SECTION -->
+        <div id="headerwrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                        <h1> <img src="favicon.ico">CraftyTech Home</h1>
+                        <!--<h2></h2>-->
+                        <div class="spacer"></div>
+                        <i class="fa fa-angle-down"></i>
+                    </div>
+                </div><!-- row -->
+            </div><!-- /container -->
+        </div><!-- /headerwrap -->
+
+        <!-- WELCOME SECTION -->
+        <div class="container">
+            <div class="row mt">
+                <div class="col-lg-8">
+                    <h1>We provide different services!!!</h1>
+                    <p>We provide different services from business applications to entertainment like games.</p>
+                </div>
+            </div><!-- /row -->
+        </div><!-- /.container -->
+
+<!-- @author : ADITYA KHARE - 70021118067 -->
+        <div class="projects" id="projects">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="projects-box">
+                            <i class="fa fa-comments" aria-hidden="true"></i><span><a href="./ChatApp/login.php">Chat App</a></span>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="projects-box">
+                            <i class="fa fa-cutlery" aria-hidden="true"></i><span><a href="./orderonline/orderonline.html">Food Order</a></span>                
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="projects-box">
+                            <i class="fa fa-quote-left" aria-hidden="true"></i><i class="fa fa-quote-right" aria-hidden="true"></i><span><a href="./Quotes/quotes.html">Quotes</a></span>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="projects-box">
+                            <i class="fa fa-gamepad" aria-hidden="true"></i><span><a href="./games.html">Games</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row mt">
+                
+            </div><!-- row -->
+        </div><!-- container -->
+
+
+        <!-- SOCIAL FOOTER --->
+        <section id="contact"></section>
+       
+        <!-- CONTACT FOOTER --->
+        <div id="cf">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <h4><strong>CONTACT</strong><br></h4>
+                        <br>
+                        <p>
+                            <strong>ADITYA KHARE</strong>
+                            <br>&ensp;&ensp;<i class="fa fa-envelope" aria-hidden="true"></i>&ensp;&ensp;<a href="mailto:aditya.khare@svkmmumbai.onmicrosoft.com">aditya.khare@svkmmumbai.onmicrosoft.com</a>
+                            <br>&ensp;&ensp;<i class="fa fa-phone" aria-hidden="true"></i>&ensp;&ensp; +91-9399860121
+                            <br>&ensp;&ensp;<i class="fa fa-linkedin" aria-hidden="true"></i>&ensp;&ensp; <a href="https://www.linkedin.com/in/aditya-khare-680766187">aditya-khare</a>
+                            <br>&ensp;&ensp;<i class="fa fa-github" aria-hidden="true"></i>&ensp;&ensp; <a href="https://github.com/Aditya-Khare">Aditya-Khare</a>
+                        </p>
+                    </div>
+                    <div class="col-lg-4">
+                        <br><br><br>
+                        <p>
+                            <strong>SAGAR PATIL</strong>
+                            <br>&ensp;&ensp;<i class="fa fa-envelope" aria-hidden="true"></i>&ensp;&ensp;<a href="mailto:sagar.patil3@svkmmumbai.onmicrosoft.com">sagar.patil3@svkmmumbai.onmicrosoft.com</a>
+                            <br>&ensp;&ensp;<i class="fa fa-phone" aria-hidden="true"></i>&ensp;&ensp; +91-8698377673
+                            <br>&ensp;&ensp;<i class="fa fa-linkedin" aria-hidden="true"></i>&ensp;&ensp; <a href="">Sagar Patil</a><br>
+                        </p>
+                    </div>
+                    <div class="col-lg-4">
+                        <br><br><br>
+                        <p>
+                            <strong>MANISH CHOUDHARY</strong>
+                            <br>&ensp;&ensp;<i class="fa fa-envelope" aria-hidden="true"></i>&ensp;&ensp;<a href="mailto:manish.choudhary@svkmmumbai.onmicrosoft.com">manish.choudhary@svkmmumbai.onmicrosoft.com</a>
+                            <br>&ensp;&ensp;<i class="fa fa-phone" aria-hidden="true"></i>&ensp;&ensp; +91-8849905788 
+                            <br>&ensp;&ensp;<i class="fa fa-linkedin" aria-hidden="true"></i>&ensp;&ensp; <a href="https://www.linkedin.com/in/manish-choudhary-18a8971b4">manish-Choudhary<br>
+                        </p>
+                    </div>
+                    <!--col-lg-4-->
+                </div><!-- row -->
+            </div><!-- container -->
+        </div><!-- Contact Footer -->
+
+<!-- @author : ADITYA KHARE - 70021118067 -->
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/main.js"></script>
+        <script src="assets/js/masonry.pkgd.min.js"></script>
+        <script src="assets/js/imagesloaded.js"></script>
+        <script src="assets/js/classie.js"></script>
+        <script src="assets/js/AnimOnScroll.js"></script>
+        <script>
+            new AnimOnScroll(document.getElementById('grid'), {
+                minDuration: 0.4,
+                maxDuration: 0.7,
+                viewportFactor: 0.2
+            });
+
+        </script>
+    </body>
+    <!-- @author : ADITYA KHARE - 70021118067 -->
+</html>
